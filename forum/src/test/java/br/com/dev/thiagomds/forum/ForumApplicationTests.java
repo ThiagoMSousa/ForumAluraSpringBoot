171bc03492bf877a981bdf16b483e48df1916f3d
Thiago Mendes <thiagomendes_95@hotmail.com>
2020-09-12 17:58:12 -0300

    Inicializando Projeto

---
+++ forum/src/test/java/br/com/dev/thiagomds/forum/ForumApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.dev.thiagomds.forum;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ForumApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
