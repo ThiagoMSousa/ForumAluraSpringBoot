c1f4dd227186d1876503980a943e5889bc50ee11
Thiago Mendes <thiagomendes_95@hotmail.com>
2020-09-12 22:32:49 -0300

    Implementação do Método TopicoDTO

---
+++ forum/src/main/java/br/com/dev/thiagomds/forum/controller/dto/TopicoDTO.java
@@ -0,0 +1,36 @@
+package br.com.dev.thiagomds.forum.controller.dto;
+
+import br.com.dev.thiagomds.forum.model.Topico;
+
+import java.time.LocalDateTime;
+
+public class TopicoDTO {
+
+    private Long id;
+    private String titulo;
+    private String mensagem;
+    private LocalDateTime dataCriacao;
+
+    public TopicoDTO(Topico topico){
+        this.id          = topico.getId();
+        this.titulo      = topico.getTitulo();
+        this.mensagem    = topico.getMensagem();
+        this.dataCriacao = topico.getDataCriacao();
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public String getMensagem() {
+        return mensagem;
+    }
+
+    public LocalDateTime getDataCriacao() {
+        return dataCriacao;
+    }
+}
