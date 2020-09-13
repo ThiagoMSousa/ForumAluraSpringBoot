ae3b2cea2d938be84db822a6ed757f914855f040
Thiago Mendes <thiagomendes_95@hotmail.com>
2020-09-12 21:18:04 -0300

    Troca da Annotation @ResponseBody para @RestController

--- forum/src/main/java/br/com/dev/thiagomds/forum/controller/TopicosController.java
+++ forum/src/main/java/br/com/dev/thiagomds/forum/controller/TopicosController.java
@@ -5,15 +5,19 @@ import br.com.dev.thiagomds.forum.model.Topico;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Arrays;
 import java.util.List;
 
-@Controller
+@RestController // A annotation @RestController nada mais é do que uma combinação das annotations @Controller e @ResponseBody.
 public class TopicosController {
 
     @RequestMapping("/topicos")
-    @ResponseBody // A anotação @ResponseBody informa a um controlador que o objeto retornado é serializado automaticamente em JSON e passado de volta para o objeto HttpResponse.
+    //@ResponseBody // A anotação @ResponseBody informa a um controlador que o objeto retornado é serializado automaticamente em JSON e passado de volta para o objeto HttpResponse.
+     /* Por padrão, o Spring considera que o retorno do método é o nome da página que ele deve carregar,
+     * mas ao utilizar a anotação @ResponseBody, indicamos que o retorno do método deve ser serializado
+     * e devolvido no corpo da resposta.*/
     public List<Topico> lista(){
         Topico topico = new Topico("Dúvida",
                 "Dúvida com Spring",
