49f9d7fd8826646908dd23510bec9b1ec9b5c431
Thiago Mendes <thiagomendes_95@hotmail.com>
2020-09-19 23:49:27 -0300

    Criado a classe implements TopicoRepository extends JpaRepository

---
+++ forum/src/main/java/br/com/dev/thiagomds/forum/repository/TopicoRepository.java
@@ -0,0 +1,10 @@
+package br.com.dev.thiagomds.forum.repository;
+
+import br.com.dev.thiagomds.forum.model.Topico;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TopicoRepository extends JpaRepository<Topico, Long> {
+
+
+
+}
