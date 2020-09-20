e3cfc9a0e1d001f6d2423d1cfa95b2c8f84ae053
Thiago Mendes <thiagomendes_95@hotmail.com>
2020-09-20 14:07:46 -0300

    Implementado método de busca por Nome do Curso na classe TopicoRepository - findByCursoNome(String nomeCurso)

--- forum/src/main/java/br/com/dev/thiagomds/forum/repository/TopicoRepository.java
+++ forum/src/main/java/br/com/dev/thiagomds/forum/repository/TopicoRepository.java
@@ -3,8 +3,10 @@ package br.com.dev.thiagomds.forum.repository;
 import br.com.dev.thiagomds.forum.model.Topico;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface TopicoRepository extends JpaRepository<Topico, Long> {
 
-
+    List<Topico> findByCursoNome(String nomeCurso);
 
 }
