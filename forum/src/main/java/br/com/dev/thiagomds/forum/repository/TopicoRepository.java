e95587cb0289681596477fbd37f10026242cfe1b
Thiago Mendes <thiagomendes_95@hotmail.com>
2020-09-20 22:54:33 -0300

    Implementado outro exemplo de busca na classe TopicoRepository

--- forum/src/main/java/br/com/dev/thiagomds/forum/repository/TopicoRepository.java
+++ forum/src/main/java/br/com/dev/thiagomds/forum/repository/TopicoRepository.java
@@ -2,6 +2,8 @@ package br.com.dev.thiagomds.forum.repository;
 
 import br.com.dev.thiagomds.forum.model.Topico;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
@@ -23,4 +25,9 @@ public interface TopicoRepository extends JpaRepository<Topico, Long> {
      */
     List<Topico> findByCursoNome(String nomeCurso);
 
+    // Nome do Método do Jeito que o Programador Gostaria
+    // Entretato, a Query deve ser criada Manualmente com JPQL
+    // @Param = :nomeCurso
+    @Query("SELECT t FROM Topico t WHERE t.curso.nome = :nomeCurso")
+    List<Topico> carregarPorNomeDoCurso(@Param("nomeCurso") String nomeCurso);
 }
