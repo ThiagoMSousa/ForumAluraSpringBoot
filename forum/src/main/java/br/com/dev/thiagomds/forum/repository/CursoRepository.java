f2ecc175629ee4739c1a7e9cf0df8ef10eb31873
Thiago Mendes <thiagomendes_95@hotmail.com>
2020-10-24 20:55:14 -0300

    Alterando o nome do atributo de 'nomeCurso' para 'nome' na Interface CursoRepository

--- forum/src/main/java/br/com/dev/thiagomds/forum/repository/CursoRepository.java
+++ forum/src/main/java/br/com/dev/thiagomds/forum/repository/CursoRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CursoRepository extends JpaRepository<Curso, Long> {
 
-    Curso findByNome( String nomeCurso );
+    Curso findByNome( String nome );
 }
