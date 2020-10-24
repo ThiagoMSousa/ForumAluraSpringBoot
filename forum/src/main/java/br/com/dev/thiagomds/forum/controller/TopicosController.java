4c2fdac1eaa2672d00404bd15e6c1b652107e7d2
Thiago Mendes <thiagomendes_95@hotmail.com>
2020-10-24 20:21:31 -0300

    Alterando a Anotação @RequestMapping para a classe TopicosController

--- forum/src/main/java/br/com/dev/thiagomds/forum/controller/TopicosController.java
+++ forum/src/main/java/br/com/dev/thiagomds/forum/controller/TopicosController.java
@@ -7,6 +7,7 @@ import br.com.dev.thiagomds.forum.repository.TopicoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,12 +15,13 @@ import java.util.Arrays;
 import java.util.List;
 
 @RestController // A annotation @RestController nada mais é do que uma combinação das annotations @Controller e @ResponseBody.
+@RequestMapping("/topicos")
 public class TopicosController {
 
     @Autowired
     private TopicoRepository topicoRepository;
 
-    @RequestMapping("/topicos")
+    //@RequestMapping("/topicos")
     //@ResponseBody // A anotação @ResponseBody informa a um controlador que o objeto retornado é serializado automaticamente em JSON e passado de volta para o objeto HttpResponse.
      /* Por padrão, o Spring considera que o retorno do método é o nome da página que ele deve carregar,
      * mas ao utilizar a anotação @ResponseBody, indicamos que o retorno do método deve ser serializado
@@ -30,10 +32,8 @@ public class TopicosController {
             return TopicoDTO.converter(topicos);
         } else {
             List<Topico> topicos = topicoRepository.findByCursoNome(nomeCurso);
-            return  TopicoDTO.converter(topicos);
+            return TopicoDTO.converter(topicos);
         }
-
-
-
     }
+
 }
